f5dd82a8ad783b99a1e98d129f20e7aad4e4c672
matsindect <matsindect@gmail.com>
2020-03-08 01:31:49 -0800

    dev console

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.web_console.whitelisted_ips = %w( 0.0.0.0 )
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blockdelta_api_production"

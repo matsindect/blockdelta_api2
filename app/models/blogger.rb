e10d0e377349b6c8cb1410ff7be01f2745922584
matsindect <matsindect@gmail.com>
2020-05-13 14:48:47 -0700

    Author slug update blogs

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -3,6 +3,7 @@ class Blogger < ApplicationRecord
     scope :filter_by_user_id, -> (user_id) { where user_id: user_id }
     belongs_to :user
     before_save :set_slug
+    after_save :update_blogs
     has_attached_file :profile_pic, 
                     :styles => { :thumb => "75x75>", :small => "150x150>" },
                     :path => 
@@ -19,9 +20,13 @@ class Blogger < ApplicationRecord
     validates_attachment :background_pic, presence: true
     do_not_validate_attachment_file_type :background_pic
 
-    
+    def update_blogs
+        @blogger = Blogger.find_by(:user_id => self.user_id)
+        Blog.where(:user_id => @blogger.user_id).update_all(author_slug: @blogger.slug)
+    end
     def set_slug
         self.slug = "#{first_name.to_s.parameterize}-#{surname.to_s.parameterize}-#{user_id.to_s}"
+        
     end 
     def can_modify_blogger?(user_id)
         role == 'admin' || role == 'blogger' &&  id.to_s == user_id.to_s
